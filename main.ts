b5b580d34023e63de4b50a75982a2074d454c357
LeaHerrera <146827129+LeaHerrera@users.noreply.github.com>
2024-10-24 17:26:03 +0200

    piedra papel y tijera con extras

--- main.ts
+++ main.ts
@@ -1,3 +1,47 @@
+let aleatorio = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    aleatorio = randint(0, 3)
+    if (aleatorio == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (aleatorio == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
 basic.forever(function () {
 	
 })
